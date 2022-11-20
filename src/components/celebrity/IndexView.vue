67946f0720dd9a51cd2f5c2d54e0bc85357975ab
cao <75364055@qq.com>
2022-11-20 17:29:50 +0800

    main frame

--- src/components/celebrity/IndexView.vue
+++ src/components/celebrity/IndexView.vue
@@ -1,7 +1,33 @@
 <template>
   <div class="common-layout">
     <el-container>
-      <el-header style="background-color: red">Header</el-header>
+      <el-header style="background-color: #0d6efd">
+        <div class="header">
+
+          <div class="function">
+            <el-button>首页</el-button>
+            <el-button>优品臻选</el-button>
+            <el-button>找合作</el-button>
+            <el-button>我的</el-button>
+          </div>
+          <div class="search">
+            <el-input style="width: 200px" placeholder="Search"></el-input>
+            <el-button>搜索</el-button>
+          </div>
+
+          <div class="exit">
+            <div>
+              <img src="../../assets/img/whb.png" alt="">
+
+            </div>
+            <div>
+              <el-button>退出</el-button>
+            </div>
+          </div>
+
+
+        </div>
+      </el-header>
       <el-main style="background-color: yellow">Main</el-main>
     </el-container>
   </div>
@@ -14,5 +40,31 @@ export default {
 </script>
 
 <style scoped>
+.header {
+  display: flex;
+  flex-direction: row;
+  margin-top: 10px;
+}
 
+.function {
+  margin-left: 100px;
+}
+
+.search {
+  margin-left: 100px;
+}
+
+.exit {
+  display: flex;
+  flex-direction: row;
+  position: absolute;
+  right: 50px;
+}
+
+.exit img {
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  margin-right: 10px;
+}
 </style>
