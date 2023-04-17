ef768f7cbf01a6d3f9d3e8afa3f73d9ee18eae78
cao <75364055@qq.com>
2023-04-17 22:27:39 +0800

    fix:myself.vue popup

--- src/components/celebrity/children/FindEshop.vue
+++ src/components/celebrity/children/FindEshop.vue
@@ -8,33 +8,123 @@
         <div>电话:{{ user.phonenumber }}</div>
         <div>常驻平台:{{ user.Platform }}</div>
         <div>
-          <button>查看详情</button>
+          <button @click="test(user)">查看详情</button>
           <button>合作</button>
         </div>
       </div>
     </div>
   </div>
+
+  <!--  弹窗-->
+  <div class="mask" v-if="centerDialogVisible">
+    <div class="box">
+      <div class="little-box">
+        <div style="width: 40px">账号</div>
+        <el-input v-model="tempInfo.arr.username" disabled placeholder="账号"/>
+      </div>
+      <div class="little-box">
+        <div style="width: 40px">昵称</div>
+        <el-input v-model="tempInfo.arr.name" placeholder="昵称"/>
+      </div>
+      <div class="little-box">
+        <div style="width: 40px">电话</div>
+        <el-input v-model="tempInfo.arr.phonenumber" placeholder="电话"/>
+      </div>
+      <div class="little-box">
+        <div style="width: 40px">住址</div>
+        <el-input v-model="input" placeholder="住址"/>
+      </div>
+      <div class="little-box">
+        <div style="width: 40px">性别</div>
+        <el-select v-model="tempInfo.arr.Sex" class="m-2" placeholder="性别" size="large">
+          <el-option
+              v-for="item in options"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value"
+          />
+        </el-select>
+      </div>
+      <div class="little-box">
+        <div style="width: 40px">年龄</div>
+        <el-input v-model="tempInfo.arr.Age" placeholder="年龄"/>
+      </div>
+      <div class="little-box">
+        <div style="width: 40px">常驻平台</div>
+        <el-input v-model="tempInfo.arr.Platform" placeholder="常驻平台"/>
+      </div>
+      <div class="little-box">
+        <div style="width: 40px">平台地址</div>
+        <el-input v-model="tempInfo.arr.PlatformUrl" placeholder="平台地址"/>
+      </div>
+      <div class="little-box">
+        <div style="width: 40px">个人简介</div>
+        <el-input v-model="tempInfo.arr.Intro" placeholder="个人简介"/>
+      </div>
+      <el-button @click="centerDialogVisible=false">关闭</el-button>
+      <el-button @click="updateInfo()">修改</el-button>
+    </div>
+
+  </div>
+
 </template>
 
 <script setup>
-import {onBeforeMount, reactive} from "vue";
+import {onBeforeMount, reactive, ref} from "vue";
 import CRequest from "@/request/CRequest";
+
+let centerDialogVisible = ref(false)
 
 let user = reactive({
   arr: []
 })
 
+let tempInfo = reactive({
+  arr: []
+})
+
+const test = (user) => {
+  console.log(centerDialogVisible)
+  tempInfo.arr = user
+  centerDialogVisible.value = true
+  console.log(centerDialogVisible)
+}
 
 onBeforeMount(() => {
   CRequest.get('/findAll').then((res) => {
     user.arr = res.data.data.data
-    console.log(user.arr)
   })
 })
 
 </script>
 
 <style scoped>
+.little-box {
+  padding: 10px;
+  margin: 3px;
+  display: flex;
+  flex-direction: row;
+}
+
+.box {
+  background: #fff;
+  padding: 40px;
+  border-radius: 8px;
+  width: 30%;
+}
+
+.mask {
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  left: 0;
+  top: 0;
+  background: rgba(0, 0, 0, 0.6);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
 .wrap {
   /*这个只是让整体在页面居中展示 */
   display: flex;
